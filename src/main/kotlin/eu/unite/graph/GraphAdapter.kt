5eb6600df0d3d83f10032709f34d6f8c0e4ddf19
Torsten Schlieder <torsten.schlieder@unite.eu>
2025-03-09 19:59:42 +0100

    Revert "Use separated link table"
    This reverts commit 4f2888c96201f3fee2f5cff5e7ddc91fe1703dbf.

--- src/main/kotlin/eu/unite/graph/GraphAdapter.kt
+++ src/main/kotlin/eu/unite/graph/GraphAdapter.kt
@@ -11,83 +11,76 @@ import eu.unite.graph.codegen.types.Book
 @DgsComponent
 class GraphAdapter {
 
-    private data class BookAuthor(val bookId: String, val authorId: String)
+    private data class AuthorRecord(val id: String, val name: String, val birth: String?, val city: String?) {
+        fun toAuthor(): Author {
+            return Author(this.id, this.name, this.birth, this.city)
+        }
+    }
+
+    private data class BookRecord(val id: String, val title: String, val year: Int, val authorId: String) {
+        fun toBook(): Book {
+            return Book(this.id, this.title, this.year)
+        }
+    }
 
     private val authorList = mutableListOf(
-        Author("1", "Kate Chopin", "1850-02-08", "St. Louis"),
-        Author("2", "Paul Auster","1947-02-03","New York"),
-        Author("3", "Jennifer Egan", "1962-09-07", "New York"),
-        Author("4", "T.C. Boyle", "1948-12-02", "Montecito CA")
+        AuthorRecord("1", "Kate Chopin", "1850-02-08", "St. Louis"),
+        AuthorRecord("2", "Paul Auster","1947-02-03","New York"),
+        AuthorRecord("3", "Jennifer Egan", "1962-09-07", "New York"),
+        AuthorRecord("4", "T.C. Boyle", "1948-12-02", "Montecito CA")
     )
 
     private val bookList = listOf(
-        Book("1", "The Awakening", 1899),
-        Book("2", "City of Glass", 1985),
-        Book("3", "Moon Palace", 1989),
-        Book("4", "The Book of Illusions", 2002),
-        Book("5", "Oracle Night", 2003),
-        Book("6", "Sunset Park", 2010),
-        Book("7", "A Visit from the Goon Squad", 2010),
-        Book("8", "Manhattan Beach", 2017),
-        Book("9", "Water Music", 1981),
-        Book("10", "Drop City", 2003),
-        Book("11", "Talk Talk", 2006)
-    )
-
-    private val bookAuthors = listOf(
-        BookAuthor("1", "1"),
-        BookAuthor("2", "2"),
-        BookAuthor("3", "2"),
-        BookAuthor("4", "2"),
-        BookAuthor("5", "2"),
-        BookAuthor("6", "2"),
-        BookAuthor("7", "3"),
-        BookAuthor("8", "3"),
-        BookAuthor("9", "4"),
-        BookAuthor("10", "4"),
-        BookAuthor("11", "4")
-
+        BookRecord("1", "The Awakening", 1899, "1"),
+        BookRecord("2", "City of Glass", 1985, "2"),
+        BookRecord("3", "Moon Palace", 1989, "2"),
+        BookRecord("4", "The Book of Illusions", 2002, "2"),
+        BookRecord("5", "Oracle Night", 2003, "2"),
+        BookRecord("6", "Sunset Park", 2010, "2"),
+        BookRecord("7", "A Visit from the Goon Squad", 2010, "3"),
+        BookRecord("8", "Manhattan Beach", 2017, "3"),
+        BookRecord("9", "Water Music", 1981, "4"),
+        BookRecord("10", "Drop City", 2003, "4"),
+        BookRecord("11", "Talk Talk", 2006, "4")
     )
 
     @DgsMutation
     fun createAuthor(name: String, birth: String?, city: String?): Author {
         val id = (authorList.size + 1).toString()
-        authorList.addLast(Author(id, name, birth, city))
-        return Author(id, name, birth, city)
+        authorList.addLast(AuthorRecord(id, name, birth, city))
+        return Author(id, name, birth, city) // No associated books yet
     }
 
     @DgsQuery(field = "authors")
     fun allAuthors(): List<Author> {
-        return authorList
+        return authorList.map { a -> a.toAuthor() }
     }
 
     @DgsQuery(field = "author")
     fun selectAuthor(id: String): Author? {
-        return authorList.find { author -> author.id == id }
+        return authorList.find { author -> author.id == id }?.toAuthor()
     }
 
     @DgsData(parentType = "Author", field = "books")
     fun authorBooks(dfe: DgsDataFetchingEnvironment): List<Book> {
         val author = dfe.getSource<Author>() ?: return emptyList()
-        return bookAuthors
-            .filter { ba -> ba.authorId == author.id }
-            .mapNotNull { ba -> bookList.find { b -> b.id == ba.bookId }}
+        return bookList.filter { b -> b.authorId == author.id }.map { b -> b.toBook() }
     }
 
     @DgsQuery(field = "books")
     fun allBooks(): List<Book> {
-        return bookList
+        return bookList.map { b -> b.toBook() }
     }
 
     @DgsQuery(field = "book")
     fun selectedBook(id: String): Book? {
-        return bookList.find { book -> book.id == id }
+        return bookList.find { book -> book.id == id }?.toBook()
     }
 
     @DgsData(parentType = "Book", field = "author")
     fun bookAuthor(dfe: DgsDataFetchingEnvironment): Author? {
         val book = dfe.getSource<Book>() ?: return null
-        val bookAuthor = bookAuthors.find { ba -> ba.bookId == book.id } ?: return null
-        return authorList.find { a -> a.id == bookAuthor.authorId }
+        val record = bookList.find { b -> b.id == book.id } ?: return null
+        return authorList.find { a -> a.id == record.authorId }?.toAuthor()
     }
 }
